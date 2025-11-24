bb9a2ec44f1cd7acb3947454a35634d76bb5589f
Kevin Wendt <wendtek@users.noreply.github.com>
2025-11-24 10:10:37 -0600

    create import error for test again

--- dags/import_errors.py
+++ dags/import_errors.py
@@ -20,8 +20,8 @@ first DAG tutorial: https://www.astronomer.io/docs/learn/get-started-with-airflo
 ![Picture of the ISS](https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2010/02/space_station_over_earth/10293696-3-eng-GB/Space_Station_over_Earth_card_full.jpg)
 """
 
-from airflow.sdk import Asset, dag, task
-#from airflow.sdk import Asset, dag
+# from airflow.sdk import Asset, dag, task
+from airflow.sdk import Asset, dag
 from pendulum import datetime
 import requests
 
