e39feafdb3a8814995a586f2f60415a61c55eefa
Kevin Wendt <wendtek@users.noreply.github.com>
2025-11-24 08:55:05 -0600

    fix import error

--- dags/import_errors.py
+++ dags/import_errors.py
@@ -22,7 +22,7 @@ first DAG tutorial: https://www.astronomer.io/docs/learn/get-started-with-airflo
 
 from airflow.sdk import Asset, dag, task
 from pendulum import datetime
-
+import requests
 
 # Define the basic parameters of the DAG, like schedule and start_date
 @dag(
@@ -80,4 +80,4 @@ def import_errors():
 
 
 # Instantiate the DAG
-example_astronauts()
+import_errors()
